a56445c8a782d4e807dd0461fbc6cfcbdcd95bad
Raktim Dhar <rikdhar313@gmail.com>
2025-07-16 20:18:57 +0530

    test

--- src/components/BlogDetails.js
+++ src/components/BlogDetails.js
@@ -1,26 +1,64 @@
 import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
 import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
+import blogsData from "../data/blogs.json";
 const ImageSource = "../src/assets/images";
+
 function BlogDetails() {
-    const { id } = useParams();
-    const [blog, setBlog] = useState(null);
-    useEffect(() => {
-        const fetchBlog = async () => {
-            try {
-                const res = await fetch("/data/blogs.json");
-                const data = await res.json();
-                const found = data.find((b) => b.id === Number(id));
-                setBlog(found || null);
-            }
-            catch (error) {
-                console.error("Failed to fetch blog data:", error);
-            }
-        };
-        fetchBlog();
-    }, [id]);
-    if (!blog)
-        return _jsx("p", { children: "Loading..." });
-    return (_jsx("div", { className: "blog-details-container", children: _jsx("div", { className: "container", children: _jsxs("div", { className: "blog-details-content-container", children: [_jsxs("div", { className: "blog-details-left-content", children: [_jsx("h1", { children: blog.title }), _jsx("div", { className: "meow", dangerouslySetInnerHTML: { __html: blog.content } }), _jsxs(Link, { to: "/products", className: "common-btn", children: ["Check Our product Out", " ", _jsx("img", { src: `${ImageSource}/arrow.svg`, alt: "" })] })] }), _jsx("div", { className: "blog-details-right-content", children: _jsx("div", { className: "sticky-image-container", children: _jsx("img", { src: blog.image, alt: blog.title, className: "img-fluid my-3" }) }) })] }) }) }));
+  const { id } = useParams();
+  const [blog, setBlog] = useState(null);
+
+  useEffect(() => {
+    // ✅ Replacing fetch with static import
+    const found = blogsData.find((b) => b.id === Number(id));
+    setBlog(found || null);
+  }, [id]);
+
+  if (!blog) return _jsx("p", { children: "Loading..." });
+
+  return _jsx("div", {
+    className: "blog-details-container",
+    children: _jsx("div", {
+      className: "container",
+      children: _jsxs("div", {
+        className: "blog-details-content-container",
+        children: [
+          _jsxs("div", {
+            className: "blog-details-left-content",
+            children: [
+              _jsx("h1", { children: blog.title }),
+              _jsx("div", {
+                className: "meow",
+                dangerouslySetInnerHTML: { __html: blog.content },
+              }),
+              _jsxs(Link, {
+                to: "/products",
+                className: "common-btn",
+                children: [
+                  "Check Our product Out ",
+                  _jsx("img", {
+                    src: `${ImageSource}/arrow.svg`,
+                    alt: "",
+                  }),
+                ],
+              }),
+            ],
+          }),
+          _jsx("div", {
+            className: "blog-details-right-content",
+            children: _jsx("div", {
+              className: "sticky-image-container",
+              children: _jsx("img", {
+                src: blog.image,
+                alt: blog.title,
+                className: "img-fluid my-3",
+              }),
+            }),
+          }),
+        ],
+      }),
+    }),
+  });
 }
+
 export default BlogDetails;
--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -2,13 +2,12 @@ import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "../assets/css/bloglist.css";
+import blogsData from "../data/blogs.json";
 function BlogList() {
     const [blogs, setBlogs] = useState([]);
     useEffect(() => {
-        fetch("/data/blogs.json")
-            .then((res) => res.json())
-            .then((data) => setBlogs(data));
-    }, []);
+  setBlogs(blogsData);
+}, []);
     return (_jsx("section", { className: "blog-list-sec", children: _jsxs("div", { className: "container", children: [_jsx("h3", { className: "sec-hdng", children: "Blog" }), _jsx("div", { className: "row", children: blogs.map((blog) => (_jsx("div", { className: "col-md-6", children: _jsxs("div", { className: "blog-card", children: [_jsx("img", { src: blog.image, alt: blog.title, className: "blog-card-img-top img-fluid" }), _jsxs("div", { className: "blog-card-body", children: [_jsx("h5", { children: blog.title }), _jsx("p", { children: blog.excerpt }), _jsx(Link, { to: `/blogs/${blog.id}`, className: "common-btn", children: "Read More" })] })] }) }, blog.id))) })] }) }));
 }
 export default BlogList;
